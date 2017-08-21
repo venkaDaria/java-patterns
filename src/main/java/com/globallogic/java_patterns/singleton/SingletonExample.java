9e148c6648e042e78781697bb7c3a13b5e2e972f
Daria Pydorenko <venka.daria@gmail.com>
2017-08-21 13:33:11 +0300

    add singleton pattern

---
+++ src/main/java/com/globallogic/java_patterns/singleton/SingletonExample.java
@@ -0,0 +1,58 @@
+package com.globallogic.java_patterns.singleton;
+
+// not lazy initialization
+class SingletonStaticField {
+    public static final SingletonStaticField INSTANCE = new SingletonStaticField();
+}
+
+// created at the time of access to the SingletonEnum class
+enum SingletonEnum {
+    INSTANCE
+}
+
+// lazy initialization (all three below)
+class SingletonSynchronizedAccessor {
+    private static SingletonSynchronizedAccessor instance;
+
+    public static synchronized SingletonSynchronizedAccessor getInstance() {
+        if (instance == null) {
+            instance = new SingletonSynchronizedAccessor();
+        }
+        return instance;
+    }
+}
+
+// only for static field
+class SingletonStaticClass {
+
+    private static class SingletonHolder {
+        private static final SingletonStaticClass HOLDER_INSTANCE = new SingletonStaticClass();
+    }
+
+    public static SingletonStaticClass getInstance() {
+        return SingletonHolder.HOLDER_INSTANCE;
+    }
+}
+
+// JDK >= 1.5
+class SingletonDoubleCheckedLocking {
+
+    // can have problems without volatile
+    private static volatile SingletonDoubleCheckedLocking instance;
+
+    public static SingletonDoubleCheckedLocking getInstance() {
+        SingletonDoubleCheckedLocking localInstance = instance;
+
+        if (localInstance == null) {
+            synchronized (SingletonDoubleCheckedLocking.class) {
+                localInstance = instance;
+
+                if (localInstance == null) {
+                    instance = localInstance = new SingletonDoubleCheckedLocking();
+                }
+            }
+        }
+
+        return localInstance;
+    }
+}
